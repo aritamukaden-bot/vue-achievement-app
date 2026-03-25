16ea75b2a57988f18b478124fdef7d0fa959a50b
さくらい <aritamukaden@gmail.com>
2026-03-25 20:39:34 +0900

    update#cardとcssの追加20260325

---
+++ src/components/Achievementcard.component.vue
@@ -0,0 +1,22 @@
+<script>
+import Achievementstatus from '@/components/Achievementstatus.component.vue'
+
+import Achievement from '@/components/Achievement.component.vue'
+
+</script>
+
+<template>
+  <div class="Achievementcard">
+<Achievementstatus/>
+<Achievement/>
+  </div>
+  <!--Tab to edit-->
+</template>
+
+<style>
+.Achievementcard{
+  background:white;
+  width:100%;
+
+}
+</style>
--- src/views/AchievementsView.vue
+++ src/views/AchievementsView.vue
@@ -1,8 +1,19 @@
-<template>
-  <h1>実績一覧</h1>
-  <AchievementComponent />
+<template
+  <div>
+    <! <h1>実績一覧</h1>
+ <AchievementComponent class="AchievementComponent"/>--Tab to edit-->
+  </div>
+ 
 </template>
 
 <script setup lang="ts">
 import AchievementComponent from '@/components/Achievement.component.vue'
 </script>
+
+
+<style>
+.AchievementComponent{
+  
+}
+  
+</style>
